04586a7c1d9cccd56ba2bcbf203cdd1575366794
Kevin Santos <3050179770117@ingenieria.usac.edu.gt>
2026-04-20 18:39:18 -0600

    Nuevos cambios app.js

---
+++ conflictosGit/apps.js
@@ -0,0 +1,10 @@
+function mostrarTareas(tareas) {
+    console.log("Lista de tareas");
+
+    tareas.forEach((tarea) => {
+        console.log("- " + tarea);
+    });
+}
+
+const tareas = ["Estudiar Git", "Hacer commits", "Aprender ramas"];
+mostrarTareas(tareas);
